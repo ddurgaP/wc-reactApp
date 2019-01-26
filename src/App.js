7c154a2d2511710cf2c5f74de920af4bf573156e
ddurgaP <prasad.durga025@gmail.com>
2019-01-26 20:25:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. v1
           <CSVLink data={csvData} >Download me</CSVLink>
         </p>
       </div>
